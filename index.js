6785f5e5d135a3efefd21c87712f96b1ab2d4183
emmaodia <odia.emma@gmail.com>
2018-12-28 18:18:58 +0100

    Set up Sender PSID

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ app.post('/webhook', (req, res) => {
       console.log(webhook_event);
     });
 
+    // Get the sender PSID
+  let sender_psid = webhook_event.sender.id;
+  console.log('Sender PSID: ' + sender_psid);
+
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
   } else {
@@ -69,3 +73,18 @@ app.get('/webhook', (req, res) => {
   //   message: "successful GET requests"
   // })
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
