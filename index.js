0cf0037067e958da00ac504e2e34fdb3d60813f7
emmaodia <odia.emma@gmail.com>
2018-12-28 11:47:40 +0100

    Updated index file

--- index.js
+++ index.js
@@ -4,40 +4,13 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
+  dotenv = require('dotenv'),
   app = express().use(bodyParser.json()); // creates express http server
 
-const dotenv = require('dotenv');
-dotenv.config();
+
+dotenv.config()
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
-});
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
@@ -63,4 +36,35 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(404);
   }
 
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+  // res.status(200).json({
+  //   message: "successful GET requests"
+  // })
 });
