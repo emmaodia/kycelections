fa47bac4a0f7436bc52e10426883fe9afce2f518
emmaodia <odia.emma@gmail.com>
2018-12-28 02:23:32 +0100

    Modified dotenv config

--- index.js
+++ index.js
@@ -4,39 +4,12 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  dotenv = require('dotenv'),
   app = express().use(bodyParser.json()); // creates express http server
 
-
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
-// Creates the endpoint for our webhook
-app.post('/webhook', (req, res) => {
-
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
@@ -64,4 +37,30 @@ app.get('/webhook', (req, res) => {
       res.sendStatus(403);
     }
   }
+});
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
 });
