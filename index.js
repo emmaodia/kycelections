ab2a7ae6866f973661af2f92b1b38dbbc437a322
emmaodia <odia.emma@gmail.com>
2018-12-30 23:17:44 +0100

    edited page token access

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   dotenv = require('dotenv'),
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
-  request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
 
-
-dotenv.config()
+dotenv.config();
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
@@ -30,18 +27,6 @@ app.post('/webhook', (req, res) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
     });
-
-    // Get the sender PSID
-  let sender_psid = webhook_event.sender.id;
-  console.log('Sender PSID: ' + sender_psid);
-
-  // Check if the event is a message or postback and
-  // pass the event to the appropriate handler function
-  if (webhook_event.message) {
-    handleMessage(sender_psid, webhook_event.message);
-  } else if (webhook_event.postback) {
-    handlePostback(sender_psid, webhook_event.postback);
-  }
 
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
@@ -65,7 +50,7 @@ app.get('/webhook', (req, res) => {
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-
+  
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
@@ -78,57 +63,4 @@ app.get('/webhook', (req, res) => {
       res.sendStatus(403);
     }
   }
-  // res.status(200).json({
-  //   message: "successful GET requests"
-  // })
 });
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-  let response;
-
-  // Check if the message contains text
-  if (received_message.text) {
-
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }
-
-  // Sends the response message
-  callSendAPI(sender_psid, response);
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
-
-}
