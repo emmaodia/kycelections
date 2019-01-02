d1d68b8bc4baf387d255dd0a9a4a1f36e3a94c4f
emmaodia <odia.emma@gmail.com>
2019-01-02 23:49:56 +0100

    Modified Index file

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   dotenv = require('dotenv'),
-  fb_token = process.env.FB_VERIFY_TOKEN,
   app = express().use(bodyParser.json()); // creates express http server
 
 dotenv.config();
@@ -45,30 +44,11 @@ app.post('/webhook', (req, res) => {
 });
 
 // Adds support for GET requests to our webhook
-// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  //let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === fb_token) {
-
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] == (process.env.VERIFY_TOKEN) ) {
+            res.status(200).send(req.query['hub.challenge']);
+        } else {
+            res.status(403).send('Error, you have passed wrong parameters')
+        }
 });
