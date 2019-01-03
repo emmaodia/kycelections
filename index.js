209a6ab04d09adea5404b81973b5970ecc91a619
emmaodia <odia.emma@gmail.com>
2019-01-03 23:11:19 +0100

    Updated Page Access token

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
   bodyParser = require('body-parser'),
   dotenv = require('dotenv'),
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
   request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
 
