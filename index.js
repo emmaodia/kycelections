0bf4e946f7778c8cd5d57e6bd7300dd7d9c16169
emmaodia <odia.emma@gmail.com>
2019-01-04 22:52:22 +0100

    Modified index file

--- index.js
+++ index.js
@@ -101,22 +101,5 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+
   }
-
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });
-
-}
