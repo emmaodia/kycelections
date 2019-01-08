8f17073d76965a09817a3100ac2207d54c1f7176
emmaodia <odia.emma@gmail.com>
2019-01-08 02:04:00 +0100

    Updated call send API to include message tag

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ function handlePostback(sender_psid, received_postback) {
 function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
+    "messaging_type": "MESSAGE_TAG"
     "recipient": {
       "id": sender_psid
     },
