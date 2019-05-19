a0796d070c1186baa4bacf601e6070f71af021d4
emmaodia <odia.emma@gmail.com>
2019-05-19 16:22:40 +0100

    Set up infoSet in routes file

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const body_parser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
-// const businessRouter = require('./routes/business');
-// const newsRouter = require('./routes/news');
-// const advertsRouter = require('./routes/adverts');
+const infoSetRouter = require('./routes/infoSet');
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
 
-//set up CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// //set up CORS
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 app.get('/api/v1/', (req, res, next) => {
@@ -24,23 +22,22 @@ app.get('/api/v1/', (req, res, next) => {
   })
 });
 
-// app.use('/api/v1/business', businessRouter);
-// app.use('/api/v1/news', newsRouter);
-// app.use('/api/v1/adverts', advertsRouter);
-//
-// //Database Setup
-// const dbConfig = require('./config/db.config.js');
-// const mongoose = require('mongoose');
-//
-// mongoose.Promise = global.Promise;
-//
-// mongoose.connect(dbConfig.url)
-// .then(()=> {
-//   console.log("Successfully connected to the database");
-// })
-// .catch(err => {
-//   console.log(`Could not connect to the database due to ${err}... Exting now!`);
-//   process.exit();
-// });
+app.use('/api/v1/infoSet', infoSetRouter)
+
+//Error Handling set up
+app.use((req, res, next) => {
+  const error = new Error('404 Page Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
