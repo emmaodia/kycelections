f5d1def2d1fde738ce10606688fd1a9cfef5a35b
emmaodia <odia.emma@gmail.com>
2019-05-19 16:58:16 +0100

    InfoSet POST feature completed

--- api/models/infoSet.js
+++ api/models/infoSet.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const infoSetSchema = mongoose.Schema({
-  _id: mongoose.Schema.Type.ObjectId,
+  _id: mongoose.Schema.Types.ObjectId,
   name: { type: String },
   office: { type: String },
   //website: : { type: String },
   // wikiLink: : { type: String },
   // state: : { type: String },
   // lga: : { type: String },
-  party: : { type: String },
+  party: { type: String },
 });
 
 mongoose.Promise = global.Promise;
--- routes/infoSet.js
+++ routes/infoSet.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const InfoSet = require('../api/models/infoSet');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: "This is the GET route"
-  })
+  console.log("Yes!");
+  process.exit();
 });
+
+router.post('/', (req, res, next) => {
+  const infoSet = new InfoSet({
+    _id: mongoose.Types.ObjectId(),
+    name: req.body.name,
+    office: req.body.office,
+    party: req.body.party
+  });
+
+  infoSet.save()
+  .then(() => {
+    res.status(201).json({
+      message: "New Info created!",
+      infoSet: {
+        _id: infoSet.id,
+        name: infoSet.name,
+        office: infoSet.office,
+        party: infoSet.party
+      }
+    })
+  })
+  .catch(error => {
+    console.log(`Info could not be created! due to: ${error}`);
+  })
+})
 
 module.exports = router;
