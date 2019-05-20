7a7dc1129e826219325ccf2452fa10f1b7fc251c
emmaodia <odia.emma@gmail.com>
2019-05-20 04:13:17 +0100

    GET ONE feature completed for infoSet

--- routes/infoSet.js
+++ routes/infoSet.js
@@ -56,4 +56,24 @@ router.post('/', (req, res, next) => {
   })
 })
 
+router.get('/:infoSet', (req, res, next) => {
+  InfoSet.findById(req.params.infoSet)
+  .exec()
+  .then(infoSet => {
+    if(!infoSet) {
+      return res,status(404).json({
+        message: 'InfoSet NOT found!'
+      })
+    }
+    res.status(200).json({
+      infoSet: infoSet
+    })
+  })
+  .catch(error => {
+    res.status(500).json({
+      error: error
+    })
+  })
+})
+
 module.exports = router;
